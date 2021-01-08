d5eefe1bfc3f823d020fe8a392515d7e2ec4a9b9
epioletti <PiolettiElizabethL@JohnDeere.com>
2021-01-08 11:09:51 -0800

    cloned files from correct repo

--- oojs.js
+++ oojs.js
@@ -6,7 +6,17 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
+class Playlist {
+  constructor(title){
+    this.title = title;
 
+  }
+  songs = [];
+  addSongs(song) { 
+    this.songs[0]=song
+
+  }
+}
 
 
 
@@ -14,8 +24,8 @@
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
-
+myPlayList = new Playlist("Heart")
+myPlayList.addSongs("arrow")
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
